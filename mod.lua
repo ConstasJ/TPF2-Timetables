01cd2eb9885668d80b03a693d8b991784313d8e2
ConstasJ <constasj@qq.com>
2025-12-17 13:08:39 +0800

    chore: update mod info

--- mod.lua
+++ mod.lua
@@ -3,15 +3,20 @@ function data()
 		info = {
 			description = _("mod_description_i18n"),
 			name = _("mod_name_i18n"),
-			minorVersion = 4,
+			minorVersion = 5,
 			severityAdd = "NONE",
 			severityRemove = "WARNING",
 			params = { },
 			tags = { "Script Mod","Script", "Timetable", "Fahrplan", "Timetables", "Fahrpläne" },
 			authors = { -- OPTIONAL one or multiple authors
 				{
+					name = "ConstasJ",
+					role = "CREATOR",
+					tfnetId = ""
+				},
+				{
 					name = "H3lyx", -- author name
-					role = "CREATOR", -- OPTIONAL "CREATOR", "CO_CREATOR", "TESTER" or "BASED_ON" or "OTHER"
+					role = "BASED_ON", -- OPTIONAL "CREATOR", "CO_CREATOR", "TESTER" or "BASED_ON" or "OTHER"
 					tfnetId = "" -- OPTIONAL train-fever-net author id
 				},
 				{
